903d27e95e1f88e09e30227d27689cb412b57d18
Piotr Staniszewski <p.stanisz@gmail.com>
2023-04-03 21:40:59 +0200

    Position_by_layout_test completed

--- tests/utils_tests.cpp
+++ tests/utils_tests.cpp
@@ -19,19 +19,25 @@ namespace watermark
     namespace test
     {
 
-        // Point position_by_layout(Layout layout, const Size &source_size, const Size &mark_size)
-        TEST(Utils_test, assert_when_empty_source_size)
+        TEST(Position_by_layout_test, assert_when_empty_source_size)
         {
             using namespace watermark;
 
             ASSERT_DEBUG_DEATH(position_by_layout(Layout::Center, Size(), Size(1, 1)), "source_size cannot be empty");
         }
 
-        TEST(Utils_test, assert_when_empty_mark_size)
+        TEST(Position_by_layout_test, assert_when_empty_mark_size)
         {
             using namespace watermark;
 
             ASSERT_DEBUG_DEATH(position_by_layout(Layout::Center, Size(1, 1), Size(0, 0)), "mark_size cannot be empty");
+        }
+
+        TEST(Position_by_layout_test, assert_when_empty_source_size_and_empty_mark_size)
+        {
+            using namespace watermark;
+
+            ASSERT_DEBUG_DEATH(position_by_layout(Layout::Center, Size(0, 0), Size(0, 0)), "cannot be empty");
         }
 
         using Position_by_layout_test_params = std::tuple<watermark::Layout, watermark::Size, watermark::Size, watermark::Point>;
@@ -54,8 +60,30 @@ namespace watermark
                 std::make_tuple(Layout::Top_left, Size(10, 10), Size(1, 1), Point(0, 0)),
                 std::make_tuple(Layout::Top_middle, Size(10, 10), Size(1, 1), Point(5, 0)),
                 std::make_tuple(Layout::Top_right, Size(10, 10), Size(1, 1), Point(9, 0)),
+                std::make_tuple(Layout::Middle_left, Size(10, 10), Size(1, 1), Point(0, 5)),
                 std::make_tuple(Layout::Center, Size(10, 10), Size(1, 1), Point(5, 5)),
-                std::make_tuple(Layout::Center, Size(10, 10), Size(10, 10), Point(0, 0))));
+                std::make_tuple(Layout::Middle_right, Size(10, 10), Size(1, 1), Point(9, 5)),
+                std::make_tuple(Layout::Bottom_left, Size(10, 10), Size(1, 1), Point(0, 9)),
+                std::make_tuple(Layout::Bottom_middle, Size(10, 10), Size(1, 1), Point(5, 9)),
+                std::make_tuple(Layout::Bottom_right, Size(10, 10), Size(1, 1), Point(9, 9)),
+                std::make_tuple(Layout::Top_left, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Top_middle, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Top_right, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Middle_left, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Center, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Middle_right, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Bottom_left, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Bottom_middle, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Bottom_right, Size(10, 10), Size(10, 10), Point(0, 0)),
+                std::make_tuple(Layout::Top_left, Size(10, 10), Size(2, 2), Point(0, 0)),
+                std::make_tuple(Layout::Top_middle, Size(10, 10), Size(2, 2), Point(4, 0)),
+                std::make_tuple(Layout::Top_right, Size(10, 10), Size(2, 2), Point(8, 0)),
+                std::make_tuple(Layout::Middle_left, Size(10, 10), Size(2, 2), Point(0, 4)),
+                std::make_tuple(Layout::Center, Size(10, 10), Size(2, 2), Point(4, 4)),
+                std::make_tuple(Layout::Middle_right, Size(10, 10), Size(2, 2), Point(8, 4)),
+                std::make_tuple(Layout::Bottom_left, Size(10, 10), Size(2, 2), Point(0, 8)),
+                std::make_tuple(Layout::Bottom_middle, Size(10, 10), Size(2, 2), Point(4, 8)),
+                std::make_tuple(Layout::Bottom_right, Size(10, 10), Size(2, 2), Point(8, 8))));
 
     }
 }
